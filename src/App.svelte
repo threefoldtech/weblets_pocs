ab4acfaf7ffd5fc049309558b71c4374ede4eea0
MohamedElmdary <engm5081@gmail.com>
2022-01-06 17:31:54 +0200

    weblet starter template

---
+++ src/App.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import Template from "./components/Template.wc.svelte";
+</script>
+
+<Template name={"Rabie"} />
---
+++ src/components/Template.wc.svelte
@@ -0,0 +1,20 @@
+<svelte:options tag="tf-template" />
+
+<script lang="ts">
+  export let name: string;
+</script>
+
+<p>
+  hello, <span>{name}</span>.
+</p>
+
+<style lang="scss" scoped>
+  p {
+    padding: 15px;
+
+    span {
+      font-family: sans-serif;
+      color: red;
+    }
+  }
+</style>
