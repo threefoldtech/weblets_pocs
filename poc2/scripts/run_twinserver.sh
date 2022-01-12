9a46b57618af320ae1775d8e80ff9fd15062bede
MElborolossy <mohammedelborolossy@gmail.com>
2022-01-12 19:35:46 +0200

    Add run scripts for msgbusd and twinserver

---
+++ poc2/scripts/run_twinserver.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+mkdir -p /tmp/vserver
+
+while getopts n:m:s: flag
+do
+    case "${flag}" in
+        n) network=${OPTARG};;
+        m) mnemonic=${OPTARG};;
+        s) storeSecret=${OPTARG};;
+    esac
+done
+
+if [[ -z $network || -z $mnemonic || -z $storeSecret ]];
+then
+    echo "Can't run twinserver, missing arguments
+- Arguments:
+    -n Choose network environment [dev or test or main]
+    -m Account mnemonics
+    -s KVStore secret"
+else
+    echo "{
+        \"network\": \"${network}\",
+        \"mnemonic\": \"${mnemonic}\",
+        \"rmb_proxy\": false,
+        \"storeSecret\": \"${storeSecret}\",
+        \"keypairType\": \"sr25519\"
+    }" > /tmp/vserver/twinserver_config.json
+    npx twinserver --config /tmp/vserver/twinserver_config.json &>> /var/log/twinserver &
+fi
